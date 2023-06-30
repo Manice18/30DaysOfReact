a3d9680a7f6b8a9a77ec91107ac9c9475474ed87
Manice18 <manishmanice2003@gmail.com>
2023-06-30 23:48:34 +0530

    change made

--- MernStackNotesApp/backend/src/server.ts
+++ MernStackNotesApp/backend/src/server.ts
@@ -1,15 +1,14 @@
-import app from "./app"
-import env from "./util/validateEnv"
+import app from "./app";
+import env from "./util/validateEnv";
 import mongoose from "mongoose";
 
-const port = 5000;
+mongoose
+  .connect(env.MONGO_URI)
+  .then(() => {
+    console.log("Mongoose connected");
+  })
+  .catch(() => console.error());
 
-mongoose.connect(env.MONGO_URI)
-        .then(()=>{
-            console.log("Mongoose connected");
-        })
-        .catch(()=>console.error())
-
-app.listen(env.PORT,()=>{
-    console.log(`Server running on port: ${port}`)
-})
+app.listen(env.PORT, () => {
+  console.log(`Server running!`);
+});
