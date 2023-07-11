dae0a3d8f5fa86545e89abe9b1fdff912fc012ae
Manice18 <manishmanice2003@gmail.com>
2023-07-12 03:52:24 +0530

    Add Day 10 project, ChatHop

--- chatapp/src/components/SidebarChatList.tsx
+++ chatapp/src/components/SidebarChatList.tsx
@@ -21,14 +21,15 @@ const SidebarChatList: FC<SidebarChatListProps> = ({ sessionId, friends }) => {
   const router = useRouter();
   const pathname = usePathname();
   const [unseenMessages, setUnseenMessages] = useState<Message[]>([]);
+  const [activeChats, setActiveChats] = useState<User[]>(friends);
 
   useEffect(() => {
     pusherClient.subscribe(toPusherKey(`user:${sessionId}:chats`));
 
     pusherClient.subscribe(toPusherKey(`user:${sessionId}:friends`));
 
-    const newFriendHandler = () => {
-      router.refresh();
+    const newFriendHandler = (newFriend: User) => {
+      setActiveChats((prev) => [...prev, newFriend]);
     };
 
     const chatHandler = (message: ExtendedMessage) => {
@@ -73,7 +74,7 @@ const SidebarChatList: FC<SidebarChatListProps> = ({ sessionId, friends }) => {
   }, [pathname]);
   return (
     <ul role="list" className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1">
-      {friends.sort().map((friend) => {
+      {activeChats.sort().map((friend) => {
         const unseenMessagesCount = unseenMessages.filter((unseenMessage) => {
           return unseenMessage.senderId === friend.id;
         }).length;
